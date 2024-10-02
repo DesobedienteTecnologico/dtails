15023434d0b9aab05f4d87048e3f93bc7475cff8
Desobediente Tecnológico <52879067+DesobedienteTecnologico@users.noreply.github.com>
2024-10-02 18:49:45 +0300

    All packages updated up to date

--- src/installer.py
+++ src/installer.py
@@ -5,15 +5,15 @@ from src.commands import *
 from src.apps import *
 
 # Variables
-bisq_url = "v1.9.15/Bisq-64bit-1.9.15"
+bisq_url = "v1.9.17/Bisq-64bit-1.9.17"
 bisq_v = bisq_url.split("/")[1]
 briar_v = "briar-desktop-debian-bullseye"
-simplex_url = "v5.8.0/simplex-desktop-ubuntu-20_04-x86_64"
+simplex_url = "v6.0.5/simplex-desktop-ubuntu-20_04-x86_64"
 simplex_v = simplex_url.split("/")[1]
 mycitadel_url = "v1.5.0/mycitadel_1.5.0-1_debian11_amd64"
 mycitadel_v = mycitadel_url.split("/")[1]
 rana_v = "v0.5.4"
-sparrow_url = "1.9.1/sparrow-1.9.1-x86_64"
+sparrow_url = "2.0.0/sparrow-2.0.0-x86_64"
 sparrow_v = sparrow_url.split("/")[1]
 specter_url = "v2.0.5/specter_desktop-v2.0.5-x86_64-linux-gnu"
 specter_v = specter_url.split("/")[1]
@@ -23,9 +23,9 @@ whirlpool_url = "62dfe35d0c82143c8fecc7d8432d4fd5/whirlpool-gui_0.10.4_amd64"
 whirlpool_v = whirlpool_url.split("/")[1]
 bitcoincore_url = "bitcoin-core-27.1/bitcoin-27.1-x86_64-linux-gnu"
 bitcoincore_v = bitcoincore_url.split("/")[1]
-feather_v = "feather-2.6.7"
-cake_v = "v4.18.2"
-liana_url = "v5.0/liana-5.0-x86_64-linux-gnu"
+feather_v = "feather-2.6.8"
+cake_v = "v4.19.5"
+liana_url = "v7.0/liana-7.0-x86_64-linux-gnu"
 liana_v = liana_url.split("/")[1]
 ############################################
 
